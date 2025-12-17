f2abd4dd840b270812a9533b191960dd2cdef6d6
Nitish k <nitish258k@gmail.com>
2025-12-17 22:16:34 +0530

    User-verification

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path , reverse_lazy
 from . import views
+from .views import CustomLoginView
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view() , name='login'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('login')) , name='logout'),
+    path('register/', views.register , name = 'register'),
     path('' , views.index, name="list"),
     path('update_task/<str:pk>/' , views.updateTask, name="update_task"),
     path('delete/<str:pk>/' , views.deleteTask, name="delete"),
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,26 +1,62 @@
 from django.shortcuts import render , redirect
-from django.http import HttpResponse
+from django.urls import reverse_lazy
 # Create your views here.
 
 from .models import *
 from .forms import *
 
-def index(request):
-    tasks = Task.objects.all()
+from django.contrib.auth.views import LoginView
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+#used when using class based functions
+
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import login
+from .forms import RegisterForm
+
+class CustomLoginView(LoginView):
+    tempelate_name = 'tasks/login.html' 
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('list')
+    
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('list')
+
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)   # auto login after register
+            return redirect('list')
+
+    context = {'form': form}
+    return render(request, 'tasks/register.html', context)
+
+@login_required
+def index(request):
+    tasks = Task.objects.filter(user=request.user)
     form = TaskForm()
 
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()
+                task = form.save(commit=False)
+                task.user = request.user
+                task.save()
         return redirect('/')
 
     context = { 'tasks' : tasks , 'form': form}
     return render(request , 'tasks/list.html', context)
 
+@login_required
 def updateTask(request , pk):
-    task = Task.objects.get(id=pk)
+    task = Task.objects.get(id=pk, user=request.user)
 
     form = TaskForm(instance=task)
 
@@ -34,8 +70,9 @@ def updateTask(request , pk):
 
     return render(request, 'tasks/update_task.html' , context)
 
+@login_required
 def deleteTask(request , pk):
-    item = Task.objects.get(id=pk)
+    item = Task.objects.get(id=pk, user=request.user)
 
     if request.method == 'POST':
         item.delete()
